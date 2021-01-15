a6395cd11e31a3fa954996fada703042c7939ce0
unknown <sharminmousumi001@gmail.com>
2021-01-14 16:09:41 -0800

    bsdj

--- src/views/KontaktaOss.vue
+++ src/views/KontaktaOss.vue
@@ -1,17 +1,26 @@
 <template>
 <div>
   <div id="Main">Kontakta Oss</div>
-<div id="address"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+<div id="address"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
   <path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-</svg> Molnvädersgatan 30,Göteborg-41835</div>
-<div id="phone"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
-</svg>  00467865439</div>
+</svg>Molnvädersgatan 30,Göteborg-41835</div>
 <div id="info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
   <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.313l6.709 3.933L8 8.928l1.291.717L16 5.715V5.4a2 2 0 0 0-1.059-1.765l-6-3.2zM16 6.873l-5.693 3.337L16 13.372v-6.5zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516zM0 13.373l5.693-3.163L0 6.873v6.5z"/>
 </svg> info@smartbilhandlare.se</div>
- 
+<div id="phone"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"/>
+</svg>             00467865439
+
+
+<div id="öppet">Öppettider: Endast Tidsbokning</div>
+<div id="öppett">Vi har oftast lediga tider samma dag</div>
+</div>
+<div id="tele1">Telefontider:</div>
+<div id="tele">Mån-Fre: 09.00 - 18.00</div>
+<div id="tele">Lördag 10.00 - 16.00</div>
+<div id="tele">Söndag 10.00 - 14.00  </div>
+
  
 </div>
 </template>
@@ -26,22 +35,70 @@ export default {
 <style lang="scss">
 #Main{
    font-family: Arial, Helvetica, sans-serif;
-    font-weight: bold;   
     font-size: 25px;
     margin-left: 10%;
     margin-top: 20px;
 
 }
+#öppet{
+   
+  margin-top: 8px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;
+}
+#öppett{
+   
+  margin-top: 3px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;
+}
 #address{
   margin-left: 30%;
-  margin-bottom: 20px;
+  margin-top:20px;
+  margin-bottom:10px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;
+  
+}
+#address svg{
+  color: brown;
+}
+#phone svg{
+  color: brown;
+}
+#info svg{
+  color: brown;
 }
 #phone{
   margin-left: 30%;
-  margin-bottom: 20px;
+  margin-bottom: 10px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;
 }
 #info{
   margin-left: 30%;
-  
+  margin-bottom: 10px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;  
 }
+#tele{
+  margin-left: 30%;
+  margin-bottom: 5px;
+}
+#tele1{
+   margin-bottom: 5px;
+  margin-top: 40px;
+  margin-left: 30%;
+   font-family: Arial, Helvetica, sans-serif;
+  font-size: 17px;
+  font-weight:bold ;
+  color:crimson;
+}
+
+
 </style>
